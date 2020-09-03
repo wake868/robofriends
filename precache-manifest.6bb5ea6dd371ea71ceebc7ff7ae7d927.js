db1a0d9ddeccc1a8b7ebbbbc590ba432003d638e
Mike Wakeland <wake868@gmail.com>
2020-09-03 11:51:23 -0700

    Updates

---
+++ precache-manifest.6bb5ea6dd371ea71ceebc7ff7ae7d927.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403db2642bdc21130f6694e657b161a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "687ae1abb7aa95e3f349",
+    "url": "/static/css/main.fcde00f7.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687ae1abb7aa95e3f349",
+    "url": "/static/js/main.df6fb410.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
